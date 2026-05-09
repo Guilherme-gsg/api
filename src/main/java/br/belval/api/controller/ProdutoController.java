aaef5651d05789dec3af47d4f9cd4bf03ebb5c38
Guilerme[D[D[D[Dherme <rm101708@estudante.fieb.edu.br>
2026-05-08 22:20:59 -0300

    delete

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -86,6 +87,25 @@ public class ProdutoController {
 	    return ResponseEntity
 	    		.status(HttpStatus.OK)
 	    		.body("Produto atualizado com sucesso!");
+	}
+	
+	@DeleteMapping("/produtos/{id}")
+	public ResponseEntity<String> apagarProduto(@PathVariable Integer id) {
+	
+	Optional<Produto> produtoOpt = repository.findById(id);
+	
+	if (produtoOpt.isEmpty()) {
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body("Produto não encontrado!");
+	}
+	
+	repository.deleteById(id);
+	
+	return ResponseEntity
+			.status(HttpStatus.OK)
+			.body("Produto apagado com sucesso!");
+		
 	}
 	
 	
